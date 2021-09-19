dc3b1e37e230f502ed044ac81207958d2beea9e7
mohammad <m.zandieh1999@gmail.com>
2021-09-20 00:40:43 +0430

    Handling Pagination With Retrofit

--- app/src/main/java/com/example/mvvmnewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -25,7 +25,7 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         val article = args.article
         webView.apply {
             webViewClient = WebViewClient()
-            loadUrl(article.url)
+            loadUrl(article?.url)
         }
 
         fab.setOnClickListener {
--- app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "0cb87c14ca24456e8cbfed96536c0198"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
