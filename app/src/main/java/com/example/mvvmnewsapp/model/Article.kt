4dbdd6986ed28c1381e4314170f7b2acd48aa904
mohammad <m.zandieh1999@gmail.com>
2022-11-22 14:52:35 +0330

    new_review_DAO_and_article_setup_part4 1401/09/1

--- app/src/main/java/com/example/mvvmnewsapp/model/Article.kt
+++ app/src/main/java/com/example/mvvmnewsapp/model/Article.kt
@@ -1,23 +1,38 @@
 package com.example.mvvmnewsapp.model
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(
+    tableName = "article"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: String? = null,
+
     @SerializedName("author")
     var author: String,
+
     @SerializedName("content")
     var content: String,
+
     @SerializedName("description")
     var description: String,
+
     @SerializedName("publishedAt")
     var publishedAt: String,
+
     @SerializedName("source")
     var source: Source,
+
     @SerializedName("title")
     var title: String,
+
     @SerializedName("url")
     var url: String,
+
     @SerializedName("urlToImage")
     var urlToImage: String
 )
