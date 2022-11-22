5a2381622373349ed8dd0867a72ce7fa52320185
mohammad <m.zandieh1999@gmail.com>
2022-11-22 23:54:00 +0330

    new_review_searching_news_part9 1401/09/1

--- app/src/main/java/com/example/mvvmnewsapp/utils/Constants.kt
+++ app/src/main/java/com/example/mvvmnewsapp/utils/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "0cb87c14ca24456e8cbfed96536c0198"
         const val  BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
